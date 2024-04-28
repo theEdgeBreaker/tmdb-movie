c7f6a171138e76623e54e80a7500ddbe2f60e57f
theEdgeBreaker <thesanjana2002@gmail.com>
2024-04-28 19:14:01 +0530

    update Footer UI

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { FaFire } from "react-icons/fa";
 import { BiMoviePlay } from "react-icons/bi";
 import { PiMonitorBold } from "react-icons/pi";
@@ -16,16 +18,17 @@ const Footer = () => {
 
   console.log(data, "");
   return (
-    <div>
+    <div className="container">
       <div className=" fixed bottom-0 w-full">
         <div className="flex justify-center items-center overflow-hidden bg-gray-800 pt-2 pb-5 sm:-b-6 md:pb-7">
           {data.map((val) => (
             <Link href={val.link} key={val.name}>
               <button
-                className=" px-7 sm:px-12 md:px-16 xl:px-24 hover:bg-gray-900 hover:bg-opacity-50 
-                 text-teal-50 rounded-md"
+                className={`px-7 sm:px-12 md:px-16 xl:px-24  hover:bg-gray-900 hover:bg-opacity-50
+                 text-teal-50 rounded-md py-2
+                  `}
               >
-                <i className="flex justify-center text-base sm:text-lg md:text-2xl pt-2">
+                <i className="flex justify-center text-base sm:text-lg md:text-2xl ">
                   {val.icon}
                 </i>
                 <h5 className="sm:font-semibold text-xs sm:text-sm md:text-lg pt-1.5 sm:pt-1">
