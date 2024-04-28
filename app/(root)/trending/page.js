914135cc58014794c6bc3640eb9a008c25cd53d5
theEdgeBreaker <thesanjana2002@gmail.com>
2024-04-28 19:16:00 +0530

    update CINESPHERE code and UI

--- app/(root)/trending/page.js
+++ app/(root)/trending/page.js
@@ -6,23 +6,25 @@ import Footer from "@/app/components/Footer";
 import TrendingMovies from "@/app/components/TrendingMovies";
 import { FaFire } from "react-icons/fa6";
 
-const page = () => {
+export default function page() {
   return (
     <div>
       <Header />
-      <div className=" bg-black  text-white min-h-screen pt-6">
+      <div className=" bg-black  text-white min-h-screen pt-5 sm:pt-6 md:pt-8">
         <div className=" w-full ">
           <div className=" flex gap-3 justify-center items-center  ">
-            <FaFire className=" text-red-600 text-4xl font-bold" />
-            <h4 className="text-3xl font-semibold underline">Trending Today</h4>
-            <FaFire className=" text-red-600 text-4xl font-bold" />
+            <FaFire className=" text-red-600 text-3xl sm:text-4xl font-bold" />
+            <h4 className=" text-2xl sm:text-3xl font-semibold underline">
+              Trending Today
+            </h4>
+            <FaFire className=" text-red-600 text-3xl sm:text-4xl font-bold" />
           </div>
-          <TrendingMovies />
         </div>
+
+        <TrendingMovies />
       </div>
+
       <Footer />
     </div>
   );
-};
-
-export default page;
+}
