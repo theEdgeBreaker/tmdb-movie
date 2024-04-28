1ff3428df440517a0c002b2db473a1819a6388b5
theEdgeBreaker <thesanjana2002@gmail.com>
2024-04-28 19:14:54 +0530

    update code

--- app/components/TrendingMovies.jsx
+++ app/components/TrendingMovies.jsx
@@ -6,14 +6,14 @@ const TrendingMovies = () => {
   const [page, setPage] = useState(1);
   const [trendingData, setTrendingData] = useState([]);
   const [totalPages, setTotalPages] = useState(1);
-  const apiKey = process.env.NEXT_PUBLIC_TRENDING_KEY; // Correctly accessing the apiKey
+
+  const apiKey = process.env.NEXT_PUBLIC_TRENDING_KEY;
 
   useEffect(() => {
     const fetchTrending = async () => {
       try {
         const response = await fetch(
           `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}&page=${page}`
-          // `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
         );
         const data = await response.json();
         setTrendingData(data.results);
@@ -31,8 +31,11 @@ const TrendingMovies = () => {
   return (
     <div
       className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
-      gap-7 md:gap-8 2xl:gap-10 px-10 md:px-14 xl:px-22 2xl:px-32 
-      pt-6 sm:pt-12 pb-72 lg:pb-60 relative"
+          gap-7 md:gap-8 2xl:gap-10 
+          px-10 md:px-14 xl:px-22 2xl:px-32 
+          pt-6 sm:pt-12 2xl:pt-16 
+          pb-72 lg:pb-60 
+          relative"
     >
       {trendingData.map((item, index) => (
         <div
