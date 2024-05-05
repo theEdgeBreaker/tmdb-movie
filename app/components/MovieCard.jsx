62d3ab27b2da1049de270e3697d4b891defd7c39
theEdgeBreaker <thesanjana2002@gmail.com>
2024-05-05 11:58:24 +0530

    Update MovieCard for vieo Trailer

--- app/components/MovieCard.jsx
+++ app/components/MovieCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MovieDetailsModal from "./MovieDetailsModal";
 
 const MovieCard = ({ item, index }) => {
@@ -18,9 +18,45 @@ const MovieCard = ({ item, index }) => {
   const img_300 = "https://image.tmdb.org/t/p/w300";
   const unavailable = "https://www.movienewz.com/img/films/poster-holder.jpg";
 
+  // const fetchMovieDetails = async (movieId) => {
+  //   const response = await fetch(
+  //     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.NEXT_PUBLIC_TRENDING_KEY}&language=en-US&append_to_response=videos`
+  //   );
+
+  //   const movieDetails = await response.json();
+  //   console.log("movie-details", movieDetails);
+
+  //   const trailerVideo = movieDetails.videos.results.find(
+  //     (video) => video.type === "Trailer"
+  //   );
+  //   // "https://vimeo.com/yourid"
+  //   // setMovie({
+  //   //   ...movieDetails,
+  //   //   video: {
+  //   //     ...trailerVideo,
+  //   //     video_url: `https://www.youtube.com/embed/${trailerVideo.key}`,
+  //   //   },
+  //   // });
+
+  //   setMovie({
+  //     ...movieDetails,
+  //     video: {
+  //       ...trailerVideo,
+  //       video_url:
+  //         trailerVideo.site === "YouTube"
+  //           ? `https://www.youtube.com/embed/${trailerVideo.key}`
+  //           : `https://vimeo.com/embed/${trailerVideo.key}`,
+  //     },
+  //   });
+  // };
+
+  // console.log(movie, "movie");
+
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center">
+    <div
+    // onClick={() => fetchMovieDetails(item.id)}
+    >
+      <div className="flex flex-col items-center justify-center cursor-pointer">
         <Image
           className="rounded-2xl  h-[19rem]  sm:h-[21.5rem] lg:h-[15.5rem] xl:h-[23.5rem] 2xl:h-[23.5rem]
           object-cover object-left-top"
@@ -28,21 +64,19 @@ const MovieCard = ({ item, index }) => {
             item.poster_path ? `${img_300}/${item.poster_path}` : unavailable
           }
           width={270}
-          style={{
-            width: "100%",
-            "@media only screen and (min-width: 769px)": {
-              width: "100px",
-            },
-          }}
           height={200}
           alt="Picture"
           onClick={handleOpen}
+          priority={true}
+          layout="responsive"
         />
 
+        {/* {isModalOpen && movie && ( */}
         {isModalOpen && (
           <MovieDetailsModal
             onClose={handleClose}
             item={item}
+            // movie={movie}
             img_300={img_300}
             unavailable={unavailable}
           />
