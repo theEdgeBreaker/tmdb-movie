eac01b9c8ea911d7de18445c44f64df69d3dced4
theEdgeBreaker <thesanjana2002@gmail.com>
2024-05-08 16:42:14 +0530

    Update header for mobile view

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,11 +3,11 @@ import { IoVideocam } from "react-icons/io5";
 
 const Header = () => {
   return (
-    <div className="flex gap-5 items-center justify-center bg-gray-800 uppercase py-3 sm:py-4 lg:py-5 ">
-      <div className=" text-teal-50 text-2xl sm:text-4xl md:text-5xl">
+    <div className="flex gap-5 items-center justify-center bg-gray-800 uppercase py-3.5 sm:py-4 lg:py-5 ">
+      <div className=" text-teal-50 text-4xl md:text-5xl">
         <IoVideocam />
       </div>
-      <div className=" text-teal-50 text-lg sm:text-2xl lg:text-4xl tracking-widest">
+      <div className=" text-teal-50 text-2xl lg:text-4xl tracking-widest">
         CineSphere
       </div>
     </div>
